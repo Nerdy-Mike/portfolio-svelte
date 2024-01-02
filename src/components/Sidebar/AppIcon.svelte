dfc6bedc888b9fba2bfbc0df0408bdd3ef89d675
Nerdy Mike <mike.thenerdy@gmail.com>
2024-01-02 11:37:51 +0700

    temp

--- src/components/Sidebar/AppIcon.svelte
+++ src/components/Sidebar/AppIcon.svelte
@@ -1,23 +1,25 @@
 <script lang="ts">
 	import { Motion, useSpring, useTransform } from 'svelte-motion';
 
-	export let mouseX;
+	export let mouseY;
 
 	let ref: HTMLDivElement;
-	let distance = useTransform(mouseX, (val: number) => {
-		let bounds = ref?.getBoundingClientRect() ?? { x: 0, width: 0 };
+	let distance = useTransform(mouseY, (val: number) => {
+			let bounds = ref?.getBoundingClientRect() ?? { y: 0, height: 0 };
 
-		return val - bounds.x - bounds.width / 2;
+			return val - bounds.y - bounds.height / 2;
 	});
 
-	let widthSync = useTransform(distance, [-150, 0, 150], [40, 100, 40]);
-	let width = useSpring(widthSync, { mass: 0.1, stiffness: 150, damping: 12 });
+	let heightSync = useTransform(distance, [-150, 0, 150], [40, 100, 40]);
+	let height = useSpring(heightSync, { mass: 0.1, stiffness: 150, damping: 12 });
+
+	$: console.log($distance);
 </script>
 
 <Motion>
 	<div
-		bind:this={ref}
-		style="width: {$width}px"
-		class="aspect-square w-10 rounded-full bg-gray-400"
+			bind:this={ref}
+			style="height: {$height}px"
+			class="aspect-square h-10 rounded-full bg-gray-400"
 	/>
 </Motion>
--- src/components/Sidebar/Dock.svelte
+++ src/components/Sidebar/Dock.svelte
@@ -1,20 +1,21 @@
 <script lang="ts">
 	import { Motion, useMotionValue } from 'svelte-motion';
 	import AppIcon from './AppIcon.svelte';
+	import { data } from './data';
 
-	let mouseX = useMotionValue(Infinity);
+	let mouseY = useMotionValue(Infinity);
 
 	const icons = [1, 2, 3, 4, 5];
 </script>
 
 <Motion>
-	<div
-		on:mousemove={(e) => mouseX.set(e.pageX)}
-		on:mouseleave={() => mouseX.set(Infinity)}
-		class="mx-auto mt-10 flex  h-16 items-end gap-4 rounded-2xl bg-gray-700 px-4 pb-3"
+	<ul
+		on:mousemove={(e) => mouseY.set(e.pageY)}
+		on:mouseleave={() => mouseY.set(Infinity)}
+		class="mx-auto my-auto pt-2 flex flex-col w-16 justify-center items-center gap-4 rounded-2xl bg-gray-700 px-4 pb-3"
 	>
 		{#each icons as _, i}
-			<AppIcon {mouseX} />
+			<AppIcon {mouseY} />
 		{/each}
-	</div>
+	</ul>
 </Motion>
