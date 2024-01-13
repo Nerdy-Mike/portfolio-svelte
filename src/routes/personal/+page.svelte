6df73717f9856b1ee5c010280f8110b2e534c92b
Nerdy Mike <mike.thenerdy@gmail.com>
2024-01-13 23:38:09 +0700

    feat spirograph protocol done

---
+++ src/routes/personal/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import MainContainer from '../../components/common/Containers/MainContainer.svelte';
+	import Grid2ColContainer from '../../components/common/Containers/Grid2ColContainer.svelte';
+	import ComponentContainer from '../../components/common/Containers/ComponentContainer.svelte';
+
+	import Spirograph from '../../components/FunStuffs/Spirograph/Spirograph.svelte';
+</script>
+
+<MainContainer>
+	<Spirograph />
+</MainContainer>
